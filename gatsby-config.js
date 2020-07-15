7173a6a529e76c5a8d25d0dc719dec020dbe7401
bill_lee <bill_lee@htc.com>
2020-07-15 17:49:14 +0800

    eslint and material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    'gatsby-plugin-eslint',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
