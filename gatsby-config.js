8b39b9b41f6d0d5af36acea129cccaeef38105de
xxlee0927 <bill_lee@htc.com>
2020-07-28 18:17:44 +0800

    netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,8 @@ module.exports = {
         },
       },
     },
-    'gatsby-plugin-eslint',
+    `gatsby-plugin-eslint`,
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
